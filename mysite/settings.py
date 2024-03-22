c7539a57be2fccf1fc28ddb6193fbba82f598ce6
Dialfe2 <chailagalindo1233@gmail.com>
2024-03-22 11:12:49 -0500

    add:data

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,9 +88,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'sql-database-modelomadurez',
+        'USER': 'usermodelomadurez',
+        'PASSWORD': '{1234ultracom}',
+        'HOST': 'sql-modelomadurez.database.windows.net',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server',
+            'MARS_Connection': True,
+        },
+    },
 }
 
 
